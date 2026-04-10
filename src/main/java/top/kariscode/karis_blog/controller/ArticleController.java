d6229a21f313876b9414821d5e023120d4bcf7a8
ISO-N <253748121+ISO-N@users.noreply.github.com>
2026-04-10 13:33:46 +0800

    feat: 按 id 查询文章

--- src/main/java/top/kariscode/karis_blog/controller/ArticleController.java
+++ src/main/java/top/kariscode/karis_blog/controller/ArticleController.java
@@ -1,9 +1,11 @@
 package top.kariscode.karis_blog.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import top.kariscode.karis_blog.entity.Article;
 import top.kariscode.karis_blog.service.ArticleService;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -18,5 +20,14 @@ public class ArticleController {
     @GetMapping("/api/articles")
     public List<Article> getArticle() {
        return articleService.findAll();
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<Article> findById(@PathVariable String id){
+        Article article = articleService.findById(id);
+        if(article==null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(article);
     }
 }
