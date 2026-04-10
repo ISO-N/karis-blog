4769e10171c56760e5a9e4cc5d31d41420acd2fc
ISO-N <253748121+ISO-N@users.noreply.github.com>
2026-04-10 12:12:52 +0800

    chore: 仓库初始化成“可运行的 Spring Boot Web 项目”

---
+++ src/main/java/top/kariscode/karis_blog/KarisBlogApplication.java
@@ -0,0 +1,13 @@
+package top.kariscode.karis_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarisBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarisBlogApplication.class, args);
+	}
+
+}
---
+++ src/main/java/top/kariscode/karis_blog/controller/HealthController.java
@@ -0,0 +1,13 @@
+package top.kariscode.karis_blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    // “健康检查”接口
+    @GetMapping("/ping")
+    public String ping(){
+        return "pong";
+    }
+}
