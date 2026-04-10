420072b95a125d8b294859a204f3b28fc7acce49
ISO-N <253748121+ISO-N@users.noreply.github.com>
2026-04-10 12:59:55 +0800

    feat: 内存版文章服务

---
+++ src/main/java/top/kariscode/karis_blog/service/ArticleService.java
@@ -0,0 +1,9 @@
+package top.kariscode.karis_blog.service;
+
+import top.kariscode.karis_blog.entity.Article;
+
+import java.util.*;
+
+public interface ArticleService {
+    public List<Article> findAll();
+}
