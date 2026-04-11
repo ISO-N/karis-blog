17fcb5cb0e72354d592d48269e18a0c9e4f2712e
ISO-N <253748121+ISO-N@users.noreply.github.com>
2026-04-11 12:31:49 +0800

    feat: 管理端新增文章接口

--- src/main/java/top/kariscode/karis_blog/controller/ArticleController.java
+++ src/main/java/top/kariscode/karis_blog/controller/ArticleController.java
@@ -1,8 +1,7 @@
 package top.kariscode.karis_blog.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import top.kariscode.karis_blog.entity.Article;
 import top.kariscode.karis_blog.service.ArticleService;
 import org.springframework.http.ResponseEntity;
@@ -43,5 +42,16 @@ public class ArticleController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(article);
+    }
+
+    // 创建文章
+    @PostMapping("/api/admin/articles")
+    public ResponseEntity<Article> createArticle(@RequestBody Article article){
+
+        if(article == null||article.getTitle() ==null||article.getSummary()==null||article.getContent()==null||article.getTitle().isBlank()||article.getSummary().isBlank()||article.getContent().isBlank()){
+            return ResponseEntity.badRequest().build();
+        }
+        Article created = articleService.create(article.getTitle(),article.getSummary(),article.getContent());
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 }
